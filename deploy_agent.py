d6661719dead77f8432b99a401d2013fa5d86ca1
prakcoin <prakhar2@gmail.com>
2026-03-03 16:37:59 -0600

    Added deployment code + files

---
+++ deploy_agent.py
@@ -0,0 +1,18 @@
+import boto3
+
+client = boto3.client('bedrock-agentcore-control')
+
+response = client.create_agent_runtime(
+    agentRuntimeName='strands_agent',
+    agentRuntimeArtifact={
+        'containerConfiguration': {
+            'containerUri': '397172001076.dkr.ecr.us-east-1.amazonaws.com/aw04-strands-agent:latest'
+        }
+    },
+    networkConfiguration={"networkMode": "PUBLIC"},
+    roleArn='arn:aws:iam::397172001076:role/AgentRuntimeRole'
+)
+
+print(f"Agent Runtime created successfully!")
+print(f"Agent Runtime ARN: {response['agentRuntimeArn']}")
+print(f"Status: {response['status']}")
---
+++ invoke_agent.py
@@ -0,0 +1,18 @@
+import boto3
+import json
+
+agent_core_client = boto3.client('bedrock-agentcore', region_name='us-east-1')
+payload = json.dumps({
+    "input": {"prompt": "What does look 1 consist of?"}
+})
+
+response = agent_core_client.invoke_agent_runtime(
+    agentRuntimeArn='arn:aws:bedrock-agentcore:us-east-1:397172001076:runtime/myaw04agent-suffix',
+    runtimeSessionId='dfmeoagmreaklgmrkleafremoigrmtesogmtrskhmtkrlshmt',  # Must be 33+ chars
+    payload=payload,
+    qualifier="DEFAULT"
+)
+
+response_body = response['response'].read()
+response_data = json.loads(response_body)
+print("Agent Response:", response_data)
